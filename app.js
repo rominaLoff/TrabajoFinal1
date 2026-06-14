39356247445aab36a8e94bf391b6bd7b2398854e
romiloff <loffhome@gmail.com>
2026-06-14 18:37:52 -0300

    Creación del carrito de compras.

--- app.js
+++ app.js
@@ -31,10 +31,13 @@ function mostrarProductos(lista) {
         btnDetalle.addEventListener("click", () => {
             abrirModal(producto);
         });
+        const btnAgregar = card.querySelector(".btn-agregar");
+        btnAgregar.addEventListener("click", () => {
+            agregarAlCarrito(producto);
+        });
         contenedor.appendChild(card);
     });
 }
-
 
 
 function abrirModal(producto) {
@@ -48,7 +51,10 @@ function abrirModal(producto) {
         <p><strong>Precio: $${producto.price}</strong></p>
         <button class="btn-agregar" id="btn-agregar-modal">Agregar al carrito</button>
     `;
-
+    const btnAgregarModal = document.querySelector("#btn-agregar-modal");
+    btnAgregarModal.addEventListener("click", () => {
+    agregarAlCarrito(producto);
+    });
     modal.style.display = "block";
 }
 
@@ -58,6 +64,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 });
 const modal = document.getElementById("modal-producto");
 const btnCerrar = document.querySelector(".cerrar-modal");
+
 if (btnCerrar && modal) {
     btnCerrar.onclick = () => {
         modal.style.display = "none";
@@ -89,12 +96,98 @@ comboCategorias.addEventListener("change", (evento) => {
     const categoriaElegida = evento.target.value;
     let urlDestino = URL_PRODUCTOS;
     if (categoriaElegida !== "todos") {
-        urlDestino = `http://fakestoreapi.com/products/category/${categoriaElegida}`;
+        urlDestino = `https://fakestoreapi.com/products/category/${categoriaElegida}`;
     }
     document.getElementById("contenedor-productos").innerHTML = "<p>Cargando productos...</p>";
     fetch(urlDestino)
     .then(respuesta => respuesta.json())
     .then(productosFiltrados => mostrarProductos(productosFiltrados));
 });
+
+let carrito = JSON.parse(localStorage.getItem("carrito-tiendita")) ||[];
+
+const btnVerCarrito = document.getElementById("btn-ver-carrito");
+const modalCarrito = document.getElementById("modal-carrito");
+const btnCerrarCarrito = document.getElementById("cerrar-carrito-btn");
+const btnVaciar = document.getElementById("btn-vaciar-carrito");
+
+function agregarAlCarrito(producto){
+carrito.push(producto);
+localStorage.setItem("carrito-tiendida", JSON.stringify(carrito));
+console.log("Producto Agregado: ", producto.title);
+actualizarContador();
+}
+
+function actualizarContador(){
+    const contador = document.getElementById("contador-carrito");
+    if (contador) {
+        contador.textContent=`🛒Carrito (${carrito.length})`;
+    }
+}
+if(btnVerCarrito){
+    btnVerCarrito.addEventListener("click", abrirModalCarrito)
+}
+
+if (btnCerrarCarrito) {
+    btnCerrarCarrito.onclick = () => {
+        modalCarrito.style.display ="none"; 
+    };
+}
+
+function abrirModalCarrito(){
+    const modalCarrito = document.getElementById("modal-carrito");
+    const contenedorItems = document.getElementById("items-carrito-contenedor");
+    const txtCantidad=document.getElementById("total-cantidad");
+    const txtPrecio=document.getElementById("total-precio");
+
+    contenedorItems.innerHTML = "";
+
+    if (carrito.length==0){
+        contenedorItems.innerHTML = "<p>El carrito está vacío.</p>";
+    }
+    else {
+        carrito.forEach((item, index) => {
+            const fila = document.createElement ("div");
+            fila.className = "item-carrito";
+
+            fila.innerHTML = `
+            <img src ="${item.image}" alt = "${item.title}" class ="img-mini-carrito"> 
+           <h4>${item.title}</h4>
+            <p><strong>$${item.price}</strong></p>
+            <button class ="btn-eliminar-item" title="Eliminar producto">🗑</button>
+            `;
+
+            const btnEliminar = fila.querySelector(".btn-eliminar-item");
+            btnEliminar.addEventListener("click", () =>{
+                eliminarDelCarrito(index);
+            });
+            contenedorItems.appendChild(fila);
+        })
+    }
+
+    txtCantidad.textContent= carrito.length;
+
+    const totalPagar = carrito.reduce((acumulador, item) => acumulador + item.price, 0);
+    txtPrecio.textContent = totalPagar.toFixed(2);
+
+    modalCarrito.style.display = "block"
+}
+
+function eliminarDelCarrito(indice) {
+    carrito.splice(indice,1);
+    localStorage.setItem("carrito-tiendita", JSON.stringify(carrito));
+    actualizarContador();
+    abrirModalCarrito();
+}
+
+if (btnVaciar) {
+    btnVaciar.addEventListener("click", ()=> {
+    carrito = [];
+    localStorage.removeItem("carrito-tiendida");
+    actualizarContador();
+    abrirModalCarrito();
+   });
+}
+
 
 
